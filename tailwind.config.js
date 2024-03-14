b7381026d532789f1235a509cecfce22a6f7d1b9
nicoll-douglas <nicoll.leo.douglas@gmail.com>
2024-03-14 05:18:54 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html", "./src/js/*.js", "./src/css/input.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
