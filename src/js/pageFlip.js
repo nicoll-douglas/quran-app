63bf45e48937a50f89b3397bd58a2b4a12bfa37f
nicoll-douglas <nicoll.leo.douglas@gmail.com>
2024-03-14 21:05:22 +0000

    feat: Allow pages can flip to contents page

--- src/js/pageFlip.js
+++ src/js/pageFlip.js
@@ -1,16 +1,29 @@
-import getAndDisplay from "./displayContent.js";
+import getAndDisplaySurah from "./displaySurah.js";
 import data from "./data.js";
+import getAndShowContents from "./showContents.js";
 
 function flipLeft() {
-  if (data.currentSurah > 1) {
-    getAndDisplay(data.currentSurah - 1);
+  if (data.currentSurah === 0) {
+    getAndDisplaySurah(114);
+    data.currentSurah = 114;
+  } else if (data.currentSurah === 1) {
+    getAndShowContents();
+    data.currentSurah = 0;
+  } else {
+    getAndDisplaySurah(data.currentSurah - 1);
     data.currentSurah -= 1;
   }
 }
 
 function flipRight() {
-  if (data.currentSurah < 114) {
-    getAndDisplay(data.currentSurah + 1);
+  if (data.currentSurah === 0) {
+    getAndDisplaySurah(1);
+    data.currentSurah = 1;
+  } else if (data.currentSurah === 114) {
+    getAndShowContents();
+    data.currentSurah = 0;
+  } else {
+    getAndDisplaySurah(data.currentSurah + 1);
     data.currentSurah += 1;
   }
 }
