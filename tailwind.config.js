2aff0218b13497c20eae70b3af0d5d5f15523201
nicoll-douglas <nicoll.leo.douglas@gmail.com>
2024-03-14 17:08:26 +0000

    Config: Edit Tailwind screens theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/index.html", "./src/js/*.js", "./src/css/input.css"],
   theme: {
     extend: {},
+    screens: {
+      md: { max: "767px" },
+    },
   },
   plugins: [],
 };
