e19a021892ea77d15a25b06eeba5d798d78aa8d6
nicoll-douglas <nicoll.leo.douglas@gmail.com>
2024-03-14 21:04:21 +0000

    Rename: "displayContent.js" to "displaySurah.js"

--- src/js/displaySurah.js
+++ src/js/displaySurah.js
@@ -1,4 +1,10 @@
-function displayContent(dataObject) {
+import data from "./data.js";
+
+function displaySurah(dataObject) {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
   // Show surah number
   document.querySelector("#surah").textContent = dataObject.data.number;
   // Show Arabic surah title
@@ -6,7 +12,7 @@ function displayContent(dataObject) {
     dataObject.data.englishName;
   // Show English surah title
   document.querySelector("#english-title").textContent =
-    dataObject.data.englishNameTranslation;
+    `"${dataObject.data.englishNameTranslation}"`;
   // Show ayahs
   document.querySelector("#container").innerHTML = "";
   dataObject.data.ayahs.forEach((ayah) => {
@@ -14,15 +20,16 @@ function displayContent(dataObject) {
     ayahElement.textContent = `${ayah.numberInSurah}. ${ayah.text}`;
     document.querySelector("#container").appendChild(ayahElement);
   });
+  data.currentSurah = dataObject.data.number;
 }
 
-export default async function getAndDisplay(surahNumber) {
+export default async function getAndDisplaySurah(surahNumber) {
   try {
     const response = await fetch(
       `http://api.alquran.cloud/v1/surah/${surahNumber}/en.asad`
     );
     const dataObject = await response.json();
-    displayContent(dataObject);
+    displaySurah(dataObject);
   } catch (err) {
     console.log(err);
   }
