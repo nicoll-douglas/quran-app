f116ef4a6bfb159e982b9268174651f22204a18a
nicoll-douglas <nicoll.leo.douglas@gmail.com>
2024-03-19 00:06:53 +0000

    File: Rename files

--- src/js/headerScrollEffect.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector("header");
-
-export default function headerScrollEffect() {
-  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-    header.classList.add("shadow-lg");
-  } else {
-    header.classList.remove("shadow-lg");
-  }
-}
--- src/js/spinner.js
+++
@@ -1,5 +0,0 @@
-export default function showSpinner() {
-  const loader = document.createElement("div");
-  loader.classList.add("loader");
-  document.querySelector("#page").appendChild(loader);
-}
