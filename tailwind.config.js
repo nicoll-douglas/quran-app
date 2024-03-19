5ac1bf10bed5ab59a96d2e4ef219d49406a52bdd
nicoll-douglas <nicoll.leo.douglas@gmail.com>
2024-03-19 00:02:59 +0000

    Refactor:  Edit content source

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/index.html", "./src/js/*.js", "./src/css/input.css"],
+  content: ["./src/index.html", "./src/js/**/*.js", "./src/css/input.css"],
   theme: {
     extend: {},
     screens: {
